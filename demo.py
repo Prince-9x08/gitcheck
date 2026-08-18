000217ca46d48c1d01f8edccfc3e0ccfe76ed766
Prince-9x08 <prince18900.2@gmail.com>
2026-08-18 14:22:51 +0530

    changed few things

--- demo.py
+++ demo.py
@@ -2,4 +2,6 @@ name="PRINCE"
 age=18
 marks=80.8
 
-print(f"my name is:{name}\nim {age} years old\ni got {marks} in 12th")
+print(f"my name is:{name}"
+      f"\nim {age}  years old"
+      f"\ni got {marks} in 12th")
